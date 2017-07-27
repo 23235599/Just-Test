e24176932d3978f8014795462f61f39585c0f50e
吴骁 <386519212@qq.com>
2017-07-27 08:41:33 +0800

    Update 5.1.3.cpp

--- 5.1.3.cpp
+++ 5.1.3.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 int main()
 {
-	double a[10] = { 28.9,32.7,45.6,78,35,86.2,27.8,43,56,65 },sum,b[10];
-	int k, i, t;
+	double bookprice[10] = { 28.9,32.7,45.6,78,35,86.2,27.8,43,56,65 },s;
+	int a[10];
         for(i=0;i<10;i++)
-		cin>>b[10];
-	sum=
+		cin>>a[10];
+	for(i=0;i<10;i++)
+		s=s+a[i]*bookprice[i];
+	cout<<s;
 }
