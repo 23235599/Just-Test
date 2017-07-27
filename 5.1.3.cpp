e963afcdf57b30164052cb79143ce45fa6475b4a
吴骁 <386519212@qq.com>
2017-07-27 08:34:59 +0800

    Update 5.1.3.cpp

--- 5.1.3.cpp
+++ 5.1.3.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 int main()
 {
-	double a[10] = { 28.9,32.7,45.6,78,35,86.2,27.8,43,56,65 },sum;
+	double a[10] = { 28.9,32.7,45.6,78,35,86.2,27.8,43,56,65 },sum,b[10];
 	int k, i, t;
-
+        for(i=0;i<10;i++)
+		cin>>b[10];
+	sum=
 }
